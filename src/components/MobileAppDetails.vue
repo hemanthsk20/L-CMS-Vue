297ea3fa61a97d9ed84200667bb451feb2bfcaff
Hemanth S K <45895550+hemanthsk20@users.noreply.github.com>
2023-02-20 18:15:35 +0530

    Update MobileAppDetails.vue

--- src/components/MobileAppDetails.vue
+++ src/components/MobileAppDetails.vue
@@ -161,7 +161,7 @@ export default {
                   `https://mobile-app-cms.azure-api.net/private/mobileApps/` + data.id,
                   data,
                   {
-                    headers: { "Ocp-Apim-Subscription-Key": "8ec096a694fa4080ba8df25a2e132205", },
+                    headers: { "Ocp-Apim-Subscription-Key": "", }, //8ec096a694fa4080ba8df25a2e132205
                   }
               )
               .then((response) => {
